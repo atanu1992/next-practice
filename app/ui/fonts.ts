585c82ea87dc29edb826a109393ac913b357eb7e
Atanu Samanta <atanusam1992@gmail.com>
2024-01-27 16:32:01 +0530

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,3 @@
+import { Inter, PT_Sans } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const Ptsans = PT_Sans({ subsets: ['latin'], weight: ['400', '700'] });
