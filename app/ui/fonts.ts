f71a05779d18a560486ab04059a8ffbc20acd1d2
Atanu Samanta <atanusam1992@gmail.com>
2024-01-27 20:02:19 +0530

    update in fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,7 @@
-import { Inter, PT_Sans } from 'next/font/google';
+import { Inter, Lusitana, PT_Sans } from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'] });
 export const Ptsans = PT_Sans({ subsets: ['latin'], weight: ['400', '700'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
